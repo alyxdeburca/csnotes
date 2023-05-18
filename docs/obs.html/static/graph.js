09bbe866ad77f67291ca4d8bf22847a36cace826
Alyx Burke <55920990+alyxdeburca@users.noreply.github.com>
2023-05-18 18:31:45 +0100

    Add files via upload

--- docs/obs.html/static/graph.js
+++ docs/obs.html/static/graph.js
@@ -3,7 +3,7 @@
 import * as grapher_2d from './graphers/2d.js';
 import * as grapher_3d from './graphers/3d.js';
 
-const CONFIGURED_HTML_URL_PREFIX = 'html';
+const CONFIGURED_HTML_URL_PREFIX = '';
 const URL_MODE = "absolute";
 
 var graphers = [
--- docs/obs.html/static/obsidian_core.js
+++ docs/obs.html/static/obsidian_core.js
@@ -5,12 +5,12 @@ var no_tab_mode = 1;
 var mermaid_enabled = 1;
 var toc_pane_div = "";
 var dir_index_pane_div = "";
-var html_url_prefix = "html";
-var CONFIGURED_HTML_URL_PREFIX = "html";
+var html_url_prefix = "";
+var CONFIGURED_HTML_URL_PREFIX = "";
 var RELATIVE_PATHS = 0;
 var documentation_mode = 0;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '49230460111073178428853033495766538605'                       // used to check whether the localStorage data is stale
+var gzip_hash = '153437035467967091334034093142110012828'                       // used to check whether the localStorage data is stale
 
 // global cache
 var fn_cache_ls_available = null;
@@ -480,7 +480,7 @@ function get_graph_data(){
     return get_html_url_prefix()+'/obs.html/data/graph.json';
 }
 function get_html_url_prefix(){
-    return 'html'  // this value is replaced by the actual url prefix when compiled
+    return ''  // this value is replaced by the actual url prefix when compiled
 }
 
 function signal_js_enabled(container){
